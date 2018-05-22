36584d09397231015309ea8f3d5833ee152bd7a9
yantang <yantang@microsoft.com>
2018-05-22 10:39:12 -0700

    Setup the project to communicate through SF remoting V2

---
+++ StatelessBackend.Interfaces/IStatelessBackendService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
+using System;
+using System.Threading.Tasks;
+
+[assembly: FabricTransportServiceRemotingProvider(RemotingListener = RemotingListener.V2Listener, RemotingClient = RemotingClient.V2Client)]
+namespace StatelessBackend.Interfaces
+{
+    public interface IStatelessBackendService : IService
+    {
+        Task<long> GetCountAsync();
+    }
+}
---
+++ WebService/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Fabric;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using StatelessBackend.Interfaces;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly StatelessServiceContext serviceContext;
+
+        public HomeController(StatelessServiceContext serviceContext)
+        {
+            this.serviceContext = serviceContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> GetStatelessBackend()
+        {
+            string serviceUri = this.serviceContext.CodePackageActivationContext.ApplicationName + "/StatelessBackend";
+
+            IStatelessBackendService proxy = ServiceProxy.Create<IStatelessBackendService>(new Uri(serviceUri));
+            long result = await proxy.GetCountAsync();
+
+            ViewData["Message"] = result;
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
