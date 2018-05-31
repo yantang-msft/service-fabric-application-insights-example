618276d11bcf39b17cc7a991e9dbdebea9b9612f
yantang <yantang@microsoft.com>
2018-05-31 10:37:33 -0700

    Add remoting V1 example

---
+++ StatelessBackendRemotingV1.Interfaces/IStatelessBackendService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatelessBackendRemotingV1.Interfaces
+{
+    public interface IStatelessBackendService : IService
+    {
+        Task<long> GetCountAsync();
+    }
+}
