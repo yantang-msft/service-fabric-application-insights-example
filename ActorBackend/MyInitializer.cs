70e052d111ebfa41a3860792b984083dd594ffab
yantang <yantang@microsoft.com>
2019-05-07 14:23:44 -0700

    Add missing file

---
+++ ActorBackend/MyInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Diagnostics;
+
+namespace ActorBackend
+{
+    class MyInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            var rt = telemetry as RequestTelemetry;
+            if (rt != null)
+            {
+                if (Activity.Current != null)
+                {
+                    foreach (var kvp in Activity.Current.Tags)
+                    {
+                        rt.Properties[kvp.Key] = kvp.Value;
+                    }
+
+                    if (Activity.Current.Id != rt.Id)
+                    {
+                        throw new Exception("Activity doesn't match the request telemetry");
+                    }
+                }
+            }
+        }
+    }
+}
