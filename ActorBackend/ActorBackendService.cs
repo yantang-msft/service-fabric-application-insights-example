63c454e9b7645258bba597ca26f0ffddbf14b189
yantang <yantang@microsoft.com>
2019-05-07 11:08:49 -0700

    Set up AI for actor service

---
+++ ActorBackend/ActorBackendService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.ServiceFabric;
+using Microsoft.ApplicationInsights.ServiceFabric.Module;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using System;
+using System.Fabric;
+
+namespace ActorBackend
+{
+    internal class ActorBackendService : ActorService
+    {
+        public ActorBackendService(
+            StatefulServiceContext context,
+            ActorTypeInformation actorTypeInfo,
+            Func<ActorService, ActorId, ActorBase> actorFactory = null,
+            Func<ActorBase, IActorStateProvider, IActorStateManager> stateManagerFactory = null,
+            IActorStateProvider stateProvider = null,
+            ActorServiceSettings settings = null)
+        : base(context, actorTypeInfo, actorFactory, stateManagerFactory, stateProvider, settings)
+        {
+            var instrumentationKey = "<app insights instrumentation key>";
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(
+                FabricTelemetryInitializerExtension.CreateFabricTelemetryInitializer(this.Context)
+            );
+
+            TelemetryConfiguration.Active.InstrumentationKey = instrumentationKey;
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new OperationCorrelationTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
+            new DependencyTrackingTelemetryModule().Initialize(TelemetryConfiguration.Active);
+            new ServiceRemotingRequestTrackingTelemetryModule().Initialize(TelemetryConfiguration.Active);
+            new ServiceRemotingDependencyTrackingTelemetryModule().Initialize(TelemetryConfiguration.Active);
+        }
+
+    }
+}
--- ActorBackend/Program.cs
+++ ActorBackend/Program.cs
@@ -22,7 +22,7 @@ namespace ActorBackend
                 // For more information, see https://aka.ms/servicefabricactorsplatform
 
                 ActorRuntime.RegisterActorAsync<ActorBackend>(
-                   (context, actorType) => new ActorService(context, actorType)).GetAwaiter().GetResult();
+                   (context, actorType) => new ActorBackendService(context, actorType)).GetAwaiter().GetResult();
 
                 Thread.Sleep(Timeout.Infinite);
             }
